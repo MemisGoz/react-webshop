ffb959488d12ded6f38da85f1e2cef9094a74d90
MemisGoz <55129239+MemisGoz@users.noreply.github.com>
2023-03-04 00:31:30 +0100

    2.3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Nav from './Components/Nav'
+import Hero from './Components/Hero';
 function App() {
   return (
+    <div>
      <Nav /> 
+     <Hero /> 
 
-
-   
+     </div>
   
   );
 }
